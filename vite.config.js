ab27ab61f1cb993d6008fd4f685a94b14fb53d93
chethankumar_03 <ckumar@gammastack.com>
2026-03-22 10:43:04 +0530

    added-config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,26 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   darkMode: "class",
-  content: ["./index.html", "./src/** /.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/.{js,ts,jsx,tsx}"],
   theme: {
     extended: {},
   },
   plugins: [react(), tailwindcss()],
+
+  build: {
+    target: "esnext",
+    minify: "terser",
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react-vendor";
+            return "vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 100,
+  },
 });
