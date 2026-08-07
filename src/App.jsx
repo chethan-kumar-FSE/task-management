e912b8cf1c4792eae746d19f70eb855499ed12b1
chethan-kumar-FSD <chethananderson4@gmail.com>
2026-08-08 01:06:49 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { TaskManagement } from "./TaskManagement/TaskManagment";
+import { useEffect } from "react";
 
 function App() {
   console.log("App.jsx renderedvasdasd");
-  console.log("done")
+  console.log("done");
+
+  useEffect(() => {
+    const fetchCall = async () => {
+      try {
+        const response = await fetch("http://13.203.210.54/user");
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchCall();
+  }, []);
   return (
     <>
       <BrowserRouter>
